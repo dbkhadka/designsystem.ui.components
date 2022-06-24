9edb8fa83f3caf361e040a23a683898d19b22c1c
Devi Khadka <Devi.Khadka@alliedelec.com>
2022-06-24 11:08:15 -0500

    Setup build for main.js

--- src/main.js
+++ src/main.js
@@ -12,3 +12,13 @@ Object.keys(Components).forEach(name => {
 export {
     CButton
 }
+
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// Vue.config.productionTip = false
+// Vue.config.devtools = true
+
+// new Vue({  
+//   render: h => h(App),
+// }).$mount('#app')
